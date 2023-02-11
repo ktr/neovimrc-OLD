b7fcf956681b06f5efa075aec7732a73bd68a858
Kevin Ryan <614956+ktr@users.noreply.github.com>
2023-02-10 20:40:35 -0500

    Bug!: Didn't test latest commits with sql helpers

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -126,6 +126,4 @@ end
 
 vim.api.nvim_set_keymap("n", "<F8>", "<cmd>lua require('flattensql').flatten_sql()<CR>", { noremap = true })
 -- reformat highlighted section with sql-formatter
-vim.api.nvim_set_keymap("x", "<leader>q", "<esc>:'<,'>!sql-formatter<CR>")
-
-xnoremap <leader>c <esc>:'<,'>:w !command<CR>
+vim.api.nvim_set_keymap("x", "<leader>q", ":!sql-formatter<CR>", {})
